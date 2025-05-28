bcf35b9689edbc05d9ac8ea5100465370effb8a5
MozesMiko <mozesmiko@gmail.com>
2025-05-28 11:16:58 +0200

    feat: added case card display logic and failsafe
    I added functions to toggle between the three display styles of the
    case cards on the "Cases" page. Function resets class of the
    "card-container" div, and adds the proper class for each display upon
    clicking the respective button.
    
    I also added "if" statements to the different functions in the script.js
    file, so that page-specific functions are only executed if the relevant
    div is present on the currently loaded page.

--- javascript/script.js
+++ javascript/script.js
@@ -43,47 +43,50 @@ window.addEventListener("resize", () => {
 
 //#region - Typewriter effect
 
-const words = ["UX Designer", "User Researcher", "Web Designer"];
-const typingSpeed = 50;
-const deletingSpeed = 50;
-const pauseBetween = 2000;
+if(document.querySelector('#typewriter')){
 
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
+  const words = ["UX Designer", "User Researcher", "Web Designer"];
+  const typingSpeed = 50;
+  const deletingSpeed = 50;
+  const pauseBetween = 2000;
 
-const typrewriter = document.getElementById("typewriter");
-const typewriterContainer = document.getElementById("typewriter-container");
+  let wordIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
 
-function setContainerHeight() {
-  const fontSize = parseInt(window.getComputedStyle(typewriter).fontSize, 10); // Get the font size of typewriter
-  typewriterContainer.style.height = `${fontSize * 1.2}px`; // Set the container height based on font size (add extra for margin)
-}
+  const typewriter = document.getElementById('typewriter');
+  const typewriterContainer = document.getElementById('typewriter-container');
 
-function type() {
-  const currentWord = words[wordIndex];
-  if (isDeleting) {
-    typewriter.textContent = currentWord.substring(0, charIndex--);
-    if (charIndex < 0) {
-      isDeleting = false;
-      wordIndex = (wordIndex + 1) % words.length;
-      setTimeout(type, typingSpeed);
+  function setContainerHeight() {
+    const fontSize = parseInt(window.getComputedStyle(typewriter).fontSize, 10); // Get the font size of typewriter
+    typewriterContainer.style.height = `${fontSize * 1.2}px`; // Set the container height based on font size (add extra for margin)
+  }
+
+  function type() {
+    const currentWord = words[wordIndex];
+    if (isDeleting) {
+      typewriter.textContent = currentWord.substring(0, charIndex--);
+      if (charIndex < 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        setTimeout(type, typingSpeed);
+      } else {
+        setTimeout(type, deletingSpeed);
+      }
     } else {
-      setTimeout(type, deletingSpeed);
-    }
-  } else {
-    typewriter.textContent = currentWord.substring(0, charIndex++);
-    if (charIndex > currentWord.length) {
-      isDeleting = true;
-      setTimeout(type, pauseBetween);
-    } else {
-      setTimeout(type, typingSpeed);
+      typewriter.textContent = currentWord.substring(0, charIndex++);
+      if (charIndex > currentWord.length) {
+        isDeleting = true;
+        setTimeout(type, pauseBetween);
+      } else {
+        setTimeout(type, typingSpeed);
+      }
     }
   }
-}
 
-setContainerHeight();
-type();
+  setContainerHeight();
+  type();
+}
 
 window.addEventListener('resize', setContainerHeight);
 
@@ -91,19 +94,56 @@ window.addEventListener('resize', setContainerHeight);
 
 //#region - Dot grid highlight
 
-const dotGrid = document.getElementById('dot-grid');
+if(document.querySelector('#dot-grid')) {
 
-dotGrid.addEventListener('mousemove', (e) => {
-  const rect = dotGrid.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-  dotGrid.style.setProperty('--x', `${x}px`);
-  dotGrid.style.setProperty('--y', `${y}px`);
-});
+  const dotGrid = document.getElementById('dot-grid');
 
-dotGrid.addEventListener('mouseleave', () => {
-  dotGrid.style.setProperty('--x', `-9999px`);
-  dotGrid.style.setProperty('--y', `-9999px`);
-});
+  dotGrid.addEventListener('mousemove', (e) => {
+    const rect = dotGrid.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    dotGrid.style.setProperty('--x', `${x}px`);
+    dotGrid.style.setProperty('--y', `${y}px`);
+  });
+
+  dotGrid.addEventListener('mouseleave', () => {
+    dotGrid.style.setProperty('--x', `-9999px`);
+    dotGrid.style.setProperty('--y', `-9999px`);
+  });
+
+}
+
+//#endregion
+
+//#region - Case card display
+
+if(document.querySelector('.card-container')) {
+
+  // const caseContainer = document.querySelector('.card-container');
+  // const largeCards = document.querySelector('#large-cards');
+  // const smallCards = document.querySelector('#view-small');
+  // const rowCards = document.querySelector('#view-row');
+
+  // smallCards.addEventListener("click", () => {
+  //   caseContainer.className = "card-container";
+  //   caseContainer.classList.add('view-small');
+  // });
+
+  // rowCards.addEventListener("click", () => {    
+  //   caseContainer.className = "card-container";
+  //   caseContainer.classList.add('view-row');
+  // });
+
+  const caseContainer = document.querySelector('.card-container');
+  const displayControlButtons = document.querySelectorAll('.display-control-btn');
+
+  for(let i = 0; i < displayControlButtons.length; i++) {
+    displayControlButtons[i].addEventListener("click", () => {
+      caseContainer.className = "card-container";
+      caseContainer.classList.add(displayControlButtons[i].id);
+    });
+  }
+
+}
 
 //#endregion
