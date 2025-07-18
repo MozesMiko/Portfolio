cf3fdc66554ad74b98dbe4463419bd15c3f7fb96
MozesMiko <62065351+MozesMiko@users.noreply.github.com>
2025-07-18 14:40:15 +0200

    feat: added info obfuscation/reveal logic, styling
    I added JavaScript code to not display my email and phone number on
    first visit. The user has to click the respective boxes to reveal that
    information. This is then saved to localstorage, so they do not need to
    click it again when revisiting of after navigating away.
    The LinkedIn and Github elements also have been changed to <a> tags and
    open the correct links in a new tab.

--- javascript/script.js
+++ javascript/script.js
@@ -200,6 +200,49 @@ window.addEventListener("scroll", () => {
 
 // #endregion
 
+// #region - Email and phone number obfuscation
+
+if(document.querySelector('.contact-page')) {
+
+const emailRevealed = localStorage.getItem("emailRevealed");
+const phoneRevealed = localStorage.getItem("phoneRevealed");
+
+const emailContact = document.getElementById('emailContact');
+const emailProper = document.getElementById('emailDisplay');
+const emailEncoded = 't92YuwWah12ZANXZ69WbuIXZ69GZ';
+const emailDecoded = atob(emailEncoded.split('').reverse().join(''));
+
+const phoneContact = document.getElementById('phoneContact');
+const phoneProper = document.getElementById('phoneDisplay');
+const phoneEncoded = 'zMDI3QDIwkDIwYDI1QzK';
+const phoneDecoded = atob(phoneEncoded.split('').reverse().join(''));
+
+if(emailRevealed === "true") {
+  emailProper.textContent = emailDecoded;
+  emailContact.classList.add('no-pointer');
+} else {
+  emailContact.addEventListener("click", () => {
+    emailProper.textContent = emailDecoded;
+    emailContact.classList.add('no-pointer');
+    localStorage.setItem("emailRevealed", true);
+  });
+};
+
+if(phoneRevealed === "true") {
+  phoneProper.textContent = phoneDecoded;
+  phoneContact.classList.add('no-pointer');
+} else {
+  phoneContact.addEventListener("click", () => {
+    phoneProper.textContent = phoneDecoded;
+    phoneContact.classList.add('no-pointer');
+    localStorage.setItem("phoneRevealed", true);
+  });
+};
+
+}
+
+// #endregion
+
 // #region - Dark theme toggle
 
 const themeToggle = document.querySelector('.theme-control');
